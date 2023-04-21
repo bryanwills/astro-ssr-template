11037123d3915d4961abe2d369a769bcf017b137
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-04-21 14:24:09 +0000

    tester

--- src/pages/endpoint.json.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-export async function post() {
-
-    const prisma = new PrismaClient()
-    const standorte = await prisma.standorte.findMany()
-
-	return standorte
-}
