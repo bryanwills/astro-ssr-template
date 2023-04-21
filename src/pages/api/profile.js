304d00aa53864d9e0ac798b2c0d5d748db3a08f7
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-04-21 14:39:37 +0000

    console ssr

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -1,13 +1,8 @@
 // Example: src/pages/api/profile.js
 import { PrismaClient } from '@prisma/client'
 export async function post(params, request) {
-    console.log("anfang")
-
     const prisma = new PrismaClient()
-    console.log("nach client")
-
     const test =prisma.standorte.findMany();
-    console.log("nach query")
     console.log(test)
 
 	return new Response(JSON.stringify({ ok: true }), {
