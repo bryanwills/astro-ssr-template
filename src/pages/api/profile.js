8ae502ef569119c3c10df0fffe82ce5eae738a84
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-04-21 14:49:57 +0000

    test

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -3,9 +3,9 @@ import { PrismaClient } from '@prisma/client'
 export async function post(params, request) {
     const prisma = new PrismaClient()
     const test = await prisma.standorte.findMany();
-    console.log(test)
+    console.log(test[0].slug)
 
-	return new Response(JSON.stringify({ ok: true }), {
+	return new Response(JSON.stringify({ ok: test }), {
 		status: 200,
 		headers: {
 			"Content-Type": "application/json",
