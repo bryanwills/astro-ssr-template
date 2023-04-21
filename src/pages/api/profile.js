9f2434481a6269addb833af6f0d3d336ed213a7b
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-04-21 15:29:18 +0000

    edge prisma

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -1,9 +1,8 @@
 // Example: src/pages/api/profile.js
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 export async function post(params, request) {
     const prisma = new PrismaClient()
     const test = await prisma.standorte.findMany();
-    console.log(test[0].slug)
 
 	return new Response(JSON.stringify({ ok: test }), {
 		status: 200,
