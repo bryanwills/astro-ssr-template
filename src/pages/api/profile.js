a532c137b7c680148bb1a5faa0ec9c24bafcaf9c
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-04-24 21:47:52 +0000

    test mit cloudflare

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client/edge'
 export async function post(params, request) {
     const prisma = new PrismaClient()
     const test = await prisma.standorte.findMany();
+	console.log(test)
 
 	return new Response(JSON.stringify({ ok: test }), {
 		status: 200,
